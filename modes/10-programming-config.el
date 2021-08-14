bab942d8f9849b9dc0ac8f89996c0635383dfb3b
Sean Patrick Hagen <sean.hagen@gmail.com>
2021-08-13 21:22:18 -0700

    Add an arrayify function

--- modes/10-programming-config.el
+++ modes/10-programming-config.el
@@ -2,6 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun arrayify (start end quote)
+    "Turn strings on newlines into a QUOTEd, comma-separated one-liner."
+    (interactive "r\nMQuote: ")
+    (let ((insertion
+           (mapconcat
+            (lambda (x) (format "%s%s%s" quote x quote))
+            (split-string (buffer-substring start end)) ", ")))
+      (delete-region start end)
+      (insert insertion)))
+
+
 (use-package projectile
   :demand
   :bind (("C-c p" . projectile-command-map))
